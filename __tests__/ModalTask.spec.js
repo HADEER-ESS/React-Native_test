69ac600ddf81699e0045bf290e4439d802f127e7
hadeer <hadeeressam21@gmail.com>
2024-05-01 23:30:35 +0300

    simple model test, and reformating code lines

--- __tests__/ModalTask.spec.js
+++ __tests__/ModalTask.spec.js
@@ -1,40 +1,73 @@
 import React from "react";
-import {render , fireEvent} from "@testing-library/react-native"
+import { render, fireEvent, waitFor } from "@testing-library/react-native"
 import ModelTask from "../src/Components/ModelTask/ModelTask";
 
 
 
-describe("Test ModalTask Component" , () => {
+describe("Test ModalTask Component", () => {
 
-    test("Default Model Task rendering" , () => {
+    test("Default Model Task rendering", () => {
 
-    const {getAllByPlaceholderText} = render(<ModelTask/>);
+        const { getAllByPlaceholderText } = render(<ModelTask />);
 
-    getAllByPlaceholderText("Enter Todo")
+        getAllByPlaceholderText("Enter Todo")
 
-})
+    })
 
-test("Show empty input error" , () => {
+    test("Show empty input error", () => {
 
-    const {getByTestId , getByText} = render(<ModelTask/>);
+        const { getByTestId, getByText } = render(<ModelTask />);
 
-    fireEvent.press(getByTestId("add_btn")) 
+        fireEvent.press(getByTestId("add_btn"))
 
-    getByText("Please Add a task")
+        getByText("Please Add a task")
 
-})
+    })
 
 
-test("Check button clicked and text input change" , () => {
+    test("Check button clicked and text input change", () => {
 
-    const { getByTestId , queryAllByText} = render(<ModelTask/>);
+        const closeModelFun = jest.fn()
 
-    fireEvent.changeText(getByTestId("Task_input") , "sdf");
-    
-    fireEvent.press(getByTestId("add_btn"));
+        const tasksState = jest.fn()
 
-    expect(queryAllByText("Please Add a task").length).toBe(0)
+        const { getByTestId, queryAllByText } = render(<ModelTask closeModelFun={closeModelFun} tasksState={tasksState} />);
 
-})
+        fireEvent.changeText(getByTestId("Task_input"), "sdf");
+
+        fireEvent.press(getByTestId("add_btn"));
+
+        expect(queryAllByText("Please Add a task").length).toBe(0)
+
+    })
+
+    test("User enter a valid data", async () => {
+
+        const setState = jest.fn();
+
+        jest.spyOn(React, "useState").mockReturnValue(['sdf', setState])
+
+        const closeModelFun = jest.fn()
+
+        const tasksState = jest.fn()
+
+        const { getByTestId } = render(<ModelTask closeModelFun={closeModelFun} tasksState={tasksState} />);
+
+
+        fireEvent.changeText(getByTestId("Task_input"), "sdf");
+
+        // expect(setState).toContain('sdf')
+
+        fireEvent.press(getByTestId("add_btn"));
+
+
+        // expect(setState).toHaveBeenCalledWith('')
+
+        expect(closeModelFun).toHaveBeenCalledTimes(1)
+
+        expect(tasksState).toHaveBeenCalled()
+
+
+    })
 })
 
