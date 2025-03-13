a974ed78f74919b889c1b2577f44ae32386a737c
hadeer <hadeeressam21@gmail.com>
2025-03-13 14:21:29 +0200

    apply the class change updates for the component that use its instance to add or display task data

--- src/Screen/TodoMainScreen.tsx
+++ src/Screen/TodoMainScreen.tsx
@@ -1,36 +1,32 @@
 import React, { useState } from 'react'
 import { FlatList, Text, TouchableOpacity, View } from 'react-native'
 import TaskItem from '../Components/TaskItem'
-import { TodoItem } from '../Types/TodoItem'
 import styles from './TodoScreenStyle.d'
 import ModelTask from '../Components/ModelTask/ModelTask'
+import Todo from '../Class/TodoClass'
 
 const TodoMainScreen = () => {
-    
-    const [tasksArray , setTasksArray] = useState<TodoItem[]>([])
-    const [openModel , setOpenModel] = useState<boolean>(false)
 
-    const pushToState = (item : TodoItem) : void => {
-        setTasksArray([...tasksArray , item])
-    }
+    const [openModel, setOpenModel] = useState<boolean>(false)
+    const todoService = Todo.getInstance()
 
-  return (
-    <View style={{padding:16}}>
-        <ModelTask tasksState={(item)=>pushToState(item)} openModelState={openModel} closeModelFun={()=>setOpenModel(false)} />
-        <View style={styles.MainScreenTitle}>
-            <Text style={styles.greetingTestStyle}>Welcome Back</Text>
-            <TouchableOpacity onPress={()=>setOpenModel(true)} style={styles.plusButtonContainer}>
-                <Text style={styles.plusTextStyle}>+</Text>
-            </TouchableOpacity>
+    return (
+        <View style={{ padding: 16 }}>
+            <ModelTask openModelState={openModel} closeModelFun={() => setOpenModel(false)} />
+            <View style={styles.MainScreenTitle}>
+                <Text style={styles.greetingTestStyle}>Welcome Back</Text>
+                <TouchableOpacity onPress={() => setOpenModel(true)} style={styles.plusButtonContainer}>
+                    <Text style={styles.plusTextStyle}>+</Text>
+                </TouchableOpacity>
+            </View>
+            <FlatList
+                data={todoService.getTodoItem()}
+                style={{ marginTop: 15 }}
+                keyExtractor={(item) => `${item.id}`}
+                renderItem={({ item }) => <TaskItem todoProp={item} />}
+            />
         </View>
-        <FlatList
-            data={tasksArray}
-            style={{marginTop:15}}
-            keyExtractor={(item) => `${item.id}`}
-            renderItem={({item}) => <TaskItem todoProp={item}/>}
-        />
-    </View>
-  )
+    )
 }
 
 export default TodoMainScreen
